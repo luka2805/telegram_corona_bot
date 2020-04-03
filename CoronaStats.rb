3609d1bb4200e34e5df6ad0cbf8e645eb6f1e1b8
Luka <lukapovreslo@gmail.com>
2020-04-03 11:12:52 +0200

    Fix wrong percentage after milion cases

--- CoronaStats.rb
+++ CoronaStats.rb
@@ -22,15 +22,13 @@ module Corona
 			end
 			
 			@countries_stats[country].each do |key, value|
-				if country == 'USA'
-					ret_string << "#{key}: #{value}\n"
-				elsif country == 'UK'
-					ret_string << "#{key}: #{value}\n"
+				if country == 'USA' or country == 'UK'
+						ret_string << "#{key}: #{value}\n"
 				else
 					ret_string << "#{key.capitalize}: #{value}\n"
 				end
 			end
-			dead_to_infected_ratio = @countries_stats[country]["Total Deaths"].sub(",", "").to_f / @countries_stats[country]["Total Cases"].sub(",", "").to_f
+			dead_to_infected_ratio = @countries_stats[country]["Total Deaths"].gsub(",", "").to_f / @countries_stats[country]["Total Cases"].gsub(",", "").to_f
 			ret_string << "Dead to infected ratio: #{(dead_to_infected_ratio * 100).round(2)}%\n"
 
 			return ret_string.join
